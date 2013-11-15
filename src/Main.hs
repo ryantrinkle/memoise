5ed5a20215909eab0ef4f89043f7766b28cd536d
Ryan Trinkle <ryan.trinkle@gmail.com>
2013-11-15 10:35:07 -0500

    Add persistence with Postgres.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 import Snap
 import Snap.Snaplet.Heist
+import Snap.Snaplet.PostgresqlSimple
 import Snap.Util.FileServe
+import Snap.Extras.TextUtils
 import Control.Lens
 import Data.Text
 import Data.Text.Encoding
@@ -10,6 +12,7 @@ import Heist.Interpreted
 
 data Memoise
   = Memoise { _heist :: Snaplet (Heist Memoise)
+            , _db :: Snaplet Postgres
             , _mainTextboxContents :: Maybe Text
             }
 makeLenses ''Memoise
@@ -17,12 +20,25 @@ makeLenses ''Memoise
 instance HasHeist Memoise where
   heistLens = subSnaplet heist
 
+instance HasPostgres (Handler Memoise Memoise) where
+  getPostgresState = with db get
+
+getUrlId :: Text -> Handler Memoise Memoise Integer
+getUrlId url = do
+  existingResults <- query "SELECT id FROM urls WHERE url = ?" (Only url)
+  case existingResults of
+    (Only urlId) : _ -> return urlId
+    [] -> do
+      [Only urlId] <- query "INSERT INTO urls (url) VALUES (?) RETURNING (id)" (Only url)
+      return urlId
+
 indexHandler :: Handler Memoise Memoise ()
 indexHandler = do
   mUrl <- getParam "url"
   case mUrl of
     Just url -> do
-      mainTextboxContents .= Just "URL Received.  What next?"
+      urlId <- getUrlId $ decodeUtf8 url
+      mainTextboxContents .= Just ("URL saved with id " <> showT urlId)
       render "index"
     Nothing -> render "index"
 
@@ -37,10 +53,12 @@ memoiseInit :: SnapletInit Memoise Memoise
 memoiseInit = makeSnaplet "memoise" "The world's laziest hyperlink shortener" Nothing $ do
   h <- nestSnaplet "heist" heist $ heistInit "templates"
   modifyHeistState $ bindAttributeSplices [("main-textbox", mainTextboxAttributeSplice)]
+  d <- nestSnaplet "db" db pgsInit
   addRoutes [ ("static", serveDirectory "static")
             , ("", indexHandler)
             ]
   return $ Memoise { _heist = h
+                   , _db = d
                    , _mainTextboxContents = Nothing
                    }
 
