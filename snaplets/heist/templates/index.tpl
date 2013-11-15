6e7d1d5566d5fd5d9b993723499e7e9d972b6cb2
Ryan Trinkle <ryan.trinkle@gmail.com>
2013-11-15 10:35:07 -0500

    Add static resources and additional markup.

--- snaplets/heist/templates/index.tpl
+++ snaplets/heist/templates/index.tpl
@@ -1,5 +1,30 @@
+<!DOCTYPE html>
 <html>
+  <head>
+    <title>memoi.se</title>
+    <link rel="stylesheet" type="text/css" href="static/bootstrap.min.css">
+    <link rel="icon" type="image/png" href="/static/favicon.png">
+  </head>
   <body>
-    Hello, world!
+    <div class="container-fluid">
+      <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+          <a class="brand" href="/">memoi.se</a>
+          <ul class="nav">
+          </ul>
+        </div>
+      </div>
+      <div class="hero-unit">
+        <h1>Hello, world!</h1>
+      </div>
+      <div class="navbar navbar-fixed-bottom container-fluid">
+        <div class="navbar-inner">
+          <a class="brand" style="padding-left:38px;"><img src="static/logo.svg"></a>
+          <ul class="nav pull-right">
+            <li class=""><a href="http://opensource.org/licenses/BSD-3-Clause">Available under BSD3 License</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
 import Snap
 import Snap.Snaplet.Heist
+import Snap.Util.FileServe
 import Control.Lens
 
--- | The Memoise type identifies our application and holds anything our snaplet needs to function.
 data Memoise
   = Memoise { _heist :: Snaplet (Heist Memoise)
             }
@@ -11,19 +11,19 @@ makeLenses ''Memoise
 instance HasHeist Memoise where
   heistLens = subSnaplet heist
 
--- | The indexHandler will be invoked whenever someone accesses the root URL, "/".
 indexHandler :: Handler Memoise Memoise ()
 indexHandler = render "index"
 
--- | Build a new Memoise snaplet.
 memoiseInit :: SnapletInit Memoise Memoise
 memoiseInit = makeSnaplet "memoise" "The world's laziest hyperlink shortener" Nothing $ do
   h <- nestSnaplet "heist" heist $ heistInit "templates"
-  addRoutes [("", indexHandler)]
+  addRoutes [ ("static", serveDirectory "static")
+            , ("", indexHandler)
+            ]
   return $ Memoise { _heist = h
                    }
 
 main :: IO ()
 main = do
-  (_, site, _) <- runSnaplet Nothing memoiseInit -- Initialize a Memoise snaplet
-  quickHttpServe site -- Start the Snap server
+  (_, site, _) <- runSnaplet Nothing memoiseInit
+  quickHttpServe site
