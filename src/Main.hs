f4c3906ba920ade9d5569550f6e8581534456240
Ryan Trinkle <ryan.trinkle@gmail.com>
2013-11-15 10:35:07 -0500

    Make the site into a Snaplet.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,20 @@
 import Snap
 
-site :: Snap ()
-site = writeText "Hello, world!"
+-- | The Memoise type identifies our application and holds anything our snaplet needs to function.
+data Memoise
+  = Memoise { } -- Right now, our application has no state variables.  Later, this will contain things like snaplets and state variables that our app needs.
+
+-- | The indexHandler will be invoked whenever someone accesses the root URL, "/".
+indexHandler :: Handler Memoise Memoise ()
+indexHandler = writeText "Hello, world!"
+
+-- | Build a new Memoise snaplet.
+memoiseInit :: SnapletInit Memoise Memoise
+memoiseInit = makeSnaplet "memoise" "The world's laziest hyperlink shortener" Nothing $ do
+  addRoutes [("", indexHandler)]
+  return $ Memoise { }
 
 main :: IO ()
-main = quickHttpServe site
+main = do
+  (_, site, _) <- runSnaplet Nothing memoiseInit -- Initialize a Memoise snaplet
+  quickHttpServe site -- Start the Snap server
